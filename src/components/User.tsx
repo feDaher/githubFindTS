69fe7e2cbb04d3e4a0a4522af370df174159d968
Felipe Daher <felipe_daher29@hotmail.com>
2023-03-23 16:34:54 -0300

    implementação User e Error

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -1,8 +1,28 @@
 import { UserProps } from "../types/user"
+import {MdLocationPin} from 'react-icons/md'
+import { Link } from "react-router-dom"
 
-const User = () => {
+const User = ({login, avatar_url, followers, following, location }: UserProps) => {
   return (
-    <div>User</div>
+    <div>
+      <img src={avatar_url} alt={login} />
+      <h2>{login}</h2>
+      {location && (
+        <p>
+        <MdLocationPin />
+        <span>{location}</span>
+       </p>
+      )}
+      <div>
+        <p>Seguidores:</p>
+        <p>{followers}</p>
+      </div>
+      <div>
+        <p>Seguindo:</p>
+        <p>{following}</p>
+      </div>
+      <Link to={`/repos/${login}`}>Ver melhores projetos</Link>
+    </div>
   )
 }
 
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,15 +1,22 @@
 import { UserProps } from '../types/user'
 import Search from '../components/Search'
 import { useState } from 'react'
-
+import User from '../components/User'
+import Error from '../components/Error'
 
 const Home = () => {
   const [user, setUser] = useState<UserProps | null>(null)
+  const [error, setError] = useState(false)
 
   const loadUser = async(userName: string) => {
+    setError(false)
+    setUser(null)
     const res = await fetch(`https://api.github.com/users/${userName}`)
     const data = await  res.json()
-    console.log(data)
+      if(res.status === 404) {
+        setError(true)
+        return
+      }
     const { avatar_url, login, location, followers, following } = data
 
     const userData: UserProps = {
@@ -24,7 +31,8 @@ const Home = () => {
   return (
     <div>
       <Search loadUser={loadUser} />
-      {user && <p>{user.login}</p>}
+      {user &&  <User {...user} /> }
+      {error && <Error />}
     </div>
   )
 }

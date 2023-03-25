52bacb50e22794f24b516edf4841687920f0ada5
Felipe Daher <felipe_daher29@hotmail.com>
2023-03-25 10:34:14 -0300

    design do repos

--- src/routes/Repos.tsx
+++ src/routes/Repos.tsx
@@ -1,7 +1,33 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import axios, { AxiosResponse } from 'axios';
+import styled from 'styled-components';
 
+const StyledList = styled.ul` 
+ display: flex;
+ flex-direction: column;
+ gap: 10px;
+ text-decoration: none;
+ list-style: none;
+ margin-top: 1rem;
+ background-color: #2b3566;
+ border-radius: 1rem;
+ padding: 1rem;
+`
+const StyledRepos = styled.li`
+  border: 1px solid #fff;
+  border-radius: 1rem;
+  padding: 10px 20px;
+  text-align: center;
+  transition: 0.5s ease-in-out;
+  cursor: pointer;
+  :hover{
+    background-color: #021b49;
+  }
+`
+const H2 = styled.h2`
+  text-align: center;
+`
 interface Repo {
   id: number;
   name: string;
@@ -20,16 +46,16 @@ const Repos = () => {
 
   return (
     <div>
-      <h2>Os melhores projetos de {login}:</h2>
-      <ul>
+      <H2>Os melhores projetos de {login}:</H2>
+      <StyledList>
         {repos.map(repo => (
-          <li key={repo.id}>
+          <StyledRepos key={repo.id}>
             <a href={repo.html_url} target="_blank" rel="noreferrer">
               {repo.name}
             </a>
-          </li>
+          </StyledRepos>
         ))}
-      </ul>
+      </StyledList>
     </div>
   );
 };
